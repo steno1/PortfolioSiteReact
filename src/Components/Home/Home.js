f36399f742e081c25265ed45f37579d87fccc1dc
steno1 <onuprinceley@gmail.com>
2023-10-04 14:47:50 +0100

    updated the Home.css

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,30 +1,32 @@
 import "./Home.css"
 
-import React from 'react'
 import Time from "../Time/Time";
 import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
     <div className="container-fluid home">
-      <div className="container home-content">
-        <div className="time"><Time/></div>
-<h1>Hi, I'm a </h1>
-<h2>
-
-<Typewriter
-  options={{
-    strings: ['Full Stack Software Developer',
-     'MERN Stack Developer', "React Developer",
-      "Web Developer", "Javascript Developer"],
-    autoStart: true,
-    loop: true,
-  }}
-/>
-  
-</h2>
+        <div className="container home-content">
+          <div className="time"><Time /></div>
+          <h1>Hi, I'm a </h1>
+          <h2>
+            <Typewriter
+              options={{
+                strings: [
+                  'Full Stack Software Developer',
+                  'MERN Stack Developer',
+                  'React Developer',
+                  'Web Developer',
+                  'Javascript Developer'
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+        </div>
       </div>
-    </div>
+    
   )
 }
 
