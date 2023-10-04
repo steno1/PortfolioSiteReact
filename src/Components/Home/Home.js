bcb89ce17bbc45daed8f6b29868f408c80f3bd56
steno1 <onuprinceley@gmail.com>
2023-10-04 11:29:09 +0100

    incoporated typewritter effect in the Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,28 @@
 import "./Home.css"
 
 import React from 'react'
+import Time from "../Time/Time";
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
     <div className="container-fluid home">
       <div className="container home-content">
-<h1>Hi I'm </h1>
+        <div className="time"><Time/></div>
+<h1>Hi, I'm a </h1>
+<h2>
+
+<Typewriter
+  options={{
+    strings: ['Full Stack Software Developer',
+     'MERN Stack Developer', "React Developer",
+      "Web Developer", "Javascript Developer"],
+    autoStart: true,
+    loop: true,
+  }}
+/>
+  
+</h2>
       </div>
     </div>
   )
