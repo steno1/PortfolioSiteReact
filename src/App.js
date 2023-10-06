c89db8cdd71e32ba4cf119c61a96fda2ba7c7483
steno1 <onuprinceley@gmail.com>
2023-10-06 05:25:40 +0100

    created About.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from './Components/About/About'
 import React from 'react'
 import Sidebar from './Components/Sidebar/Sidebar'
 
@@ -5,6 +6,7 @@ const App = () => {
   return (
  <>
  <Sidebar/>
+ <About/>
  </>
   )
 }
